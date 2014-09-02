685fe88adf59910ae9ff59c46b60f41bd79c152f
Md. Jahidul Hamid <jahidulhamid@yahoo.com>
2014-09-02 10:43:34 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-read -s -p "Enter Password: " mypassword
-echo $mypassword | sudo -S cp -R firmware/Wi-FI/* /lib/firmware/
-echo $mypassword | sudo -S cp -R firmware/BT/* /lib/firmware/
+sudo cp -R firmware/Wi-FI/* /lib/firmware/
+sudo cp -R firmware/BT/* /lib/firmware/
 cd rt2x00
 make clean
 make
@@ -9,12 +8,9 @@ make clean
 make
 ##build complete
 cd ..
-echo $mypassword | sudo -S mkdir -p /usr/local/MT7630E
-echo $mypassword | sudo -S cp -R rt2x00/*.ko /usr/local/MT7630E
-echo $mypassword | sudo -S cp -R btloader/*.ko /usr/local/MT7630E
-echo $mypassword | sudo -S cp rt2x00/load.sh /etc/init.d/
-echo $mypassword | sudo -S update-rc.d load.sh defaults
-echo $mypassword | sudo -S ./rt2x00/load.sh
-
-
-
+sudo mkdir -p /usr/local/MT7630E
+sudo cp -R rt2x00/*.ko /usr/local/MT7630E
+sudo cp -R btloader/*.ko /usr/local/MT7630E
+sudo cp rt2x00/load.sh /etc/init.d/
+sudo update-rc.d load.sh defaults
+sudo ./rt2x00/load.sh
